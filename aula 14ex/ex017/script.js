d8a13d9325955c439a988ee27823c84cb1c1ec39
Natalia <nataliamaria868@gmail.com>
2024-01-26 20:16:41 -0300

    Complemento

--- aula 14ex/ex017/script.js
+++ aula 14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert("Olá!")
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = '' // limpando a area antes de garar outra tabuada
+        while (c <= 10) {
+            let item = document.createElement('option') // Cria o elemento option dinamicaente
+            item.text = `${n} x ${c} = ${n*c}` // texto escrito de cada item
+            item.value = `tab${c}` // Criando  um  value para cada option
+            tab.appendChild(item) // Adicionar um elemento Filho
+            c++
+        }
+    }
+}
